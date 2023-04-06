00d6f4c3eff1248484d0cee58f65b487f983eea8
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-04-05 22:28:15 -0500

    Initial commit

---
+++ dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,26 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_1.calculate()}!');
+}
+
+// Comment lines
+/* Toggle vlog comment */
+/* In Dart you can declare a varible in two ways the first is: */
+
+int number=10;//This way you don´t set null value because  it way use the strong typing
+
+// ignore: prefer_typing_uninitialized_variables
+var numero;//In this case the variable is using the "weak typing"
+
+
+//And the varible types are:
+//Integer
+int number2=2;//Always end with -> ;
+//String
+String string2="Hola mundo";
+//Double
+double double1=1.0;
+//Boolean
+bool boolean1=true;
+
